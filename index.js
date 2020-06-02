45f1bae6c6c0f2740661565d26bb688a36a1db9b
Chase <sde.chase.rogers@gmail.com>
2020-06-02 13:12:19 -0600

    added line breaks in printout

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const printNumber = num => {
 }
 console.log("Write a for loop that prints to the console the numbers 0 through 9.")
 printNumber(9);
+console.log("\n");
 
 const printNumberBackwards = num => {
     for (let i = num; i >= 0; i--) {
@@ -14,6 +15,7 @@ const printNumberBackwards = num => {
 }
 console.log("Write a for loop that prints to the console 9 through 0.")
 printNumberBackwards(9);
+console.log("\n");
 
 const fruit = ["banana", "orange", "apple", "kiwi"];
 
@@ -24,6 +26,7 @@ const printArray = arr => {
 }
 console.log('Write a for loop that prints these fruits to the console. var fruit = ["banana", "orange", "apple", "kiwi"]')
 printArray(fruit);
+console.log("\n");
 
 // Going for Bronze!!!!!
 const pushToArray = (num, arr) => {
@@ -34,6 +37,7 @@ const pushToArray = (num, arr) => {
 }
 console.log("Write a for loop that will push the numbers 0 through 9 to an array.")
 pushToArray(9, []);
+console.log("\n");
 
 const printEven = num => {
     for (let i = 0; i <= num; i++) {
@@ -44,6 +48,7 @@ const printEven = num => {
 }
 console.log("Write a for loop that prints to the console only even numbers 0 through 100.")
 printEven(100);
+console.log("\n");
 
 const fruits = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
 
@@ -55,6 +60,7 @@ const pushFruit = (arr, newArray) => {
 }
 console.log('Write a for loop that will push every other fruit to an array. var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]')
 pushFruit(fruits, []);
+console.log("\n");
 
 // Going for Silver!!!!!
 var peopleArray = [
@@ -83,6 +89,7 @@ const printNames = arrayOfObjects => {
 }
 console.log("Write a loop that will print out all the names of the people of the people array.")
 printNames(peopleArray);
+console.log("\n");
 
 const pushJobsAndNames = (arr, arrayOfNames, arrayOfJobs) => {
     for (let i = 0; i < arr.length; i++) {
@@ -94,6 +101,7 @@ const pushJobsAndNames = (arr, arrayOfNames, arrayOfJobs) => {
 }
 console.log("Write a loop that pushes the names into a names array, and the occupations into an occupations array.")
 pushJobsAndNames(peopleArray, [], []);
+console.log("\n");
 
 const pushJobsAndNames2 = (arr, arrayOfNames, arrayOfJobs) => {
     for (let i = 0; i < arr.length; i++) {
@@ -108,6 +116,7 @@ const pushJobsAndNames2 = (arr, arrayOfNames, arrayOfJobs) => {
 }
 console.log('Write a loop that pushes every other name to an array starting with the first person, in this case "Harrison Ford", and every other occupation to another array starting with, in this case, "Singer".')
 pushJobsAndNames2(peopleArray, [], []);
+console.log("\n");
 
 // Going for Gold!!!!!!
 const makeNestedArray1 = (numOfElements, numOfArrays, matrix) => {
@@ -123,6 +132,7 @@ const makeNestedArray1 = (numOfElements, numOfArrays, matrix) => {
 }
 console.log('Create an array that mimics a grid like the following using nested for loops:[[0, 0, 0], [0, 0, 0], [0, 0, 0]]')
 makeNestedArray1(3, 3, []);
+console.log("\n");
 
 const makeNestedArray2 = (numOfElements, numOfArrays, matrix) => {
     for (let i = 0; i < numOfArrays; i++) {
@@ -137,6 +147,7 @@ const makeNestedArray2 = (numOfElements, numOfArrays, matrix) => {
 }
 console.log('Create an array that mimics a grid like the following using nested for loops:[[0, 0, 0], [1, 1, 1], [2, 2, 2]]')
 makeNestedArray2(3, 3, []);
+console.log("\n");
 
 const makeNestedArray3 = (numOfElements, numOfArrays, matrix) => {
     for (let i = 0; i < numOfArrays; i++) {
@@ -151,6 +162,7 @@ const makeNestedArray3 = (numOfElements, numOfArrays, matrix) => {
 }
 console.log('Create an array that mimics a grid like the following using nested for loops:[[0, 1, 2], [0, 1, 2], [0, 1, 2]]')
 makeNestedArray3(3, 3, []);
+console.log("\n");
 
 const makeNestedArrayAllX = (num, arr) => {
     for (let i = 0; i < num; i++) {
